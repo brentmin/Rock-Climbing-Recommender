943c64ee946990376ce968bd3c86a047305abf50
Eric Liu <twigoracle@gmail.com>
2021-01-09 22:13:12 -0800

    Initial commit
    File strucure and .gitignore

---
+++ run.py
@@ -0,0 +1,17 @@
+# Brian Cheng
+# Eric Liu
+# Brent Min
+
+# run.py is a file that 
+# [TODO]
+
+def main():
+    """
+    TODO
+    """
+
+    pass
+
+# run.py cannot be imported as a module
+if __name__ == '__main__':
+    main()
