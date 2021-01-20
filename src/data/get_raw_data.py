bb60ed6a0b825efd2e0e3989dd2dd3bd0e548318
Eric Liu <ericliu121@yahoo.com>
2021-01-20 15:57:57 -0800

    Expanded scraping (#3)
    * Added notebook code to get user ratings for each route
    
    * Added code to modify the original url
    
    * Added and integrated function get_route_rating_data
    
    * Added most documentation
    
    Co-authored-by: Eric Liu <twigoracle@gmail.com>

--- src/data/get_raw_data.py
+++ src/data/get_raw_data.py
@@ -15,12 +15,16 @@ from src.functions import make_absolute
 def get_raw_data(data_params):
     """
     This function collates all scraping logic
+
+    :param:     data_params     A dictionary containing all data parameters. The only one used is
+                                the location at which to save raw data
     """
     # store raw data here
     raw_data = []
 
     # for now, just scrape Yosemite routes
     area_url = "https://www.mountainproject.com/area/105833381/yosemite-national-park"
+    # area_url = "https://www.mountainproject.com/area/113603736/the-box" # only 7 climbs!
     all_routes = find_all_routes_in_area(area_url)
 
     # for every route in Yosemite, get the route data
@@ -35,39 +39,124 @@ def get_route_data(route_url):
     """
     Get all route data for a single route
 
-    TODO: get the users who rated and their ratings for this route
-
     :param:     route_url   The URL at which the route lives
+
+    :return:    dict        A dictionary containing the following information:
+                            "@context": N/A
+                            "@type": N/A
+                            "name": Name of the climb
+                            "description": Description of the climb
+                            "image": link to the climb image
+                            "geo": dict with lat/long
+                            "aggregateRating": dict with average rating/number of ratings
+                            "route_url": url at which the route can be found
+                            "user_ratings": dict with key user_id and value user_rating (0-4)
     """
-    # TODO: document function
+    # get the climb description
     text = requests.get(route_url).text
     soup = BeautifulSoup(text, 'html.parser')
+
+    # split up the stuff we want
     data = json.loads("".join(soup.find("script", {"type":"application/ld+json"}).contents))
     description = soup.find('div', {'class': 'fr-view'}).contents
     data['description'] = ''.join([x for x in description if isinstance(x, str)])
     data['route_url'] = route_url
+
+    # if there exists more than zero ratings for this climb, then get those user(s)
+    if(int(data["aggregateRating"]["reviewCount"]) > 0):
+        data["user_ratings"] = get_route_rating_data(route_url)
+    # otherwise just add an empty dict
+    else:
+        data["user_ratings"] = {}
+
     return data
+
+def get_route_rating_data(route_url):
+    """
+    Get all user_ids and the ratings for the input route
+
+    :param:     route_url   The URL at which the route lives. NOTE (!) this is not the URL at which
+                            rating data can be found, it is modified in this function
+
+    :return:    dict        A dictionary containing the key of user_id and value of user rating
+                            for the input climb url
+    """
+    # first modify the route_url to access the stats
+    route_stats_url = route_url.split("/")
+    route_stats_url.insert(4, "stats")
+    route_stats_url = "/".join(route_stats_url)
+
+    # get the html of the stats page
+    text = requests.get(route_stats_url).text
+    soup = BeautifulSoup(text, 'html.parser')
+
+    # get the first table which should contain ratings
+    ratings_table = soup.find("table", attrs={"class": "table"})
+
+    # make sure that a table exists 
+    if(ratings_table == None):
+        print(route_stats_url)
+        return {}
+
+    # get the rows of the rating table
+    rows = ratings_table.find_all("tr")
+    
+    # store ratings here
+    user_ratings = {}
+
+    # every single row is a users rating
+    for row in rows:
+        # first col contains the user_id
+        cols = row.find_all("td")
+        user_url = cols[0].find("a").get("href")
+        user_id = user_url.split("/")[4]
+
+        # second col contains the user rating (0-4 stars)
+        stars = len(cols[1].find_all("img"))
+
+        # the above interprets avoid (single image of a bomb) the same as one star (single image
+        # of a star)
+        # adjust for that
+        if(stars == 1 and ("bomb" in cols[1].find("img")["src"])):
+            stars = 0
+        
+        # store the rating
+        user_ratings[user_id] = stars
+            
+    # return the ratings
+    return user_ratings
 
 def get_user_history(user_url):
     """
     Get all routes this user has climbed
-
-    TODO: get the ratings for each route?
+    Note: this function is not used
 
     :param:     user_url    The URL of the user profile
+
+    :return:    list        A list containing the url of all climbs this user has rated
     """
-    # TODO: document function
+    # store links here
     links = []
+
+    # get the html for the user
     text = requests.get(user_url + '/ticks').text
     soup = BeautifulSoup(text, 'html.parser')
+
+    # iterate over each page of climbs that the user has done
     num_pages = int(soup.find_all('a', {"class":"no-click"})[2].contents[0].strip()[-1])
     for i in range(num_pages):
         text = requests.get(user + '/ticks?page=' + str(i + 1)).text
+
+        # parse the page of climbs
         soup = BeautifulSoup(text, 'html.parser')
+
+        # get and store all links to the climbs the user has done
         all_links = soup.find_all('a')
         for link in all_links:
             if len(link.find_all('strong')) > 0 and len(link) < 2:
                 links.append({link.find('strong').contents[0]: link.get('href')})
+
+    # return the list of links
     return links
 
 def find_all_routes_in_area(area_url):
