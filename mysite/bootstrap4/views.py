bb9f78094db64ca281fe0398e0e1b3d2b955f12d
Eric Liu <ericliu121@yahoo.com>
2021-01-26 19:21:19 -0800

    Website intergration (#6)
    * First try:
    
    * Fixed path issue
    
    * Added a p tag to modify
    
    * Start of run.py integration
    
    * Documentation
    
    Co-authored-by: Eric Liu <twigoracle@gmail.com>

--- mysite/bootstrap4/views.py
+++ mysite/bootstrap4/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+import sys
+sys.path.append('../../..')
+
 def bootstrap4_index(request):
-    return render(request, 'index.html', {})
+    # enter the if if the button is pressed on the website
+    if(request.method == "POST" and "run_script" in request.POST):
+        # import the main function and run it with the selected options
+        from run import main
+        config = {"mongodb": True, "top_pop": True}
+        result = main(config)
+
+        # return the template but with the returned contents of main
+        return render(request, 'index.html', {"test": result})
+
+    return render(request, 'index.html', {"test": "click above button to see the return of run.py"})
--- run.py
+++ run.py
@@ -20,6 +20,11 @@ def main(params=None):
     :param:     params      Optional command line arguments in dictionary form. If not None, then
                             command line arguments will be ignored
     """
+    # params will only be not None if this function is called from the website
+    # in that case, change the behavior of the script accordingly
+    if(params != None):
+        return str(params)
+
     # all command line arguments
     # for a description of the arguments, refer to the README.md or run "python run.py -h"
     parser = argparse.ArgumentParser()
