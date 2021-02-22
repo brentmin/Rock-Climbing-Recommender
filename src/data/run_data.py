2acfa88bf440503dd2295c174fbffeedf54536a8
Brian Cheng <brc042@ucsd.edu>
2021-02-21 21:32:23 -0800

    add delete and upload parameters

--- src/data/run_data.py
+++ src/data/run_data.py
@@ -9,6 +9,8 @@ from src.data.get_clean_data import get_clean_data
 from src.data.upload_data import upload_data
 from src.functions import *
 
+from pymongo import MongoClient
+
 def run_data(data_params, args):
     """
     This function collates all scraping and cleaning code into a single function called by run.py
@@ -17,8 +19,12 @@ def run_data(data_params, args):
     :param:     args            The command line input parameters. This tells us whether or not to
                                 run data scraping/cleaning code
     """
+    #the mongo connection
+    client_url = 'mongodb+srv://DSC102:coliniscool@cluster0.4gstr.mongodb.net/MountainProject?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE'
+    my_client = MongoClient(client_url)
+
     # first check that we want to run some data scraping/cleaning code
-    if(args["data"] or args["clean"]):
+    if(args["data"] or args["clean"] or args["delete"] or args["upload"]):
         # TODO: Delete the data folders in order to empty them, since if the user requests that data
         #       scraping code be run, then overwrite existing data
 
@@ -34,6 +40,9 @@ def run_data(data_params, args):
         if(args["clean"]):
             get_clean_data(data_params)
 
-        #if(args["upload"]):
-        #    upload_data(data_params)
+        if(args["delete"]):
+            print('gone gone gone')
+
+        if(args["upload"]):
+            upload_data(data_params, my_client)
     
