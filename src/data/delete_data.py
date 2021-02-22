64e1314087e3743570a117d791c5c5a44ed7875d
Brian Cheng <brc042@ucsd.edu>
2021-02-21 21:32:28 -0800

    Create delete_data.py

---
+++ src/data/delete_data.py
@@ -0,0 +1,17 @@
+# Brian Cheng
+# Eric Liu
+# Brent Min
+
+#wipes out data from mongodb
+
+import json 
+import csv
+
+from pymongo import MongoClient
+
+
+def upload_data(data_params, my_client):
+    climbs = myclient.MountainProject.climbs
+
+    climbs.remove()
+    
