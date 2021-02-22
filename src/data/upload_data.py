804606d750d517c279e7e466d3c652267bd0dece
Brian Cheng <brc042@ucsd.edu>
2021-02-21 21:44:24 -0800

    Update upload_data.py

--- src/data/upload_data.py
+++ src/data/upload_data.py
@@ -9,21 +9,14 @@ import csv
 import pandas as pd
 
 from pymongo import MongoClient
-
+from src.functions import *
 
 def upload_data(data_params, my_client):
-    climbs = myclient.MountainProject.climbs
-
-    data_path = make_absolute(data_params["clean_data_folder"])
-
-    # Turns the csv into a list of dicts, each dict is a row
-    # each key in the dict is a column names
-    with open(data_path) as f:
-        df = pd.read_csv('/Users/brian/Desktop/Rock-Climbing-Recommender/data/cleaned/climbs.csv')
-        data = df.to_dict('records')
-
-    # Loops through and attempts to upload each row data onto mongo
-    # if duplicate in mongo, replace with itself
-    # if not, then insert data
-    for entry in data:
+    #uploads the cleaned climbs data to MondoDB
+    climbs = my_client.MountainProject.climbs
+    climbs_data_path = make_absolute(data_params["clean_data_folder"] + 'climbs.csv')
+    with open(climbs_data_path) as f:
+        climbs_df = pd.read_csv(f)
+        climbs_data = climbs_df.to_dict('records')
+    for entry in climbs_data:
         climbs.replace_one(entry, entry, upsert=True)
