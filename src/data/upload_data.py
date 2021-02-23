6fc6ecf0a51f6d4708d9908816118a31405b3ee1
Brian Cheng <brc042@ucsd.edu>
2021-02-23 01:05:40 -0800

    revert changes

--- src/data/upload_data.py
+++ src/data/upload_data.py
@@ -17,7 +17,6 @@ def upload_data(data_params, my_client):
     climbs_data_path = make_absolute(data_params["clean_data_folder"] + 'climbs.csv')
     with open(climbs_data_path) as f:
         climbs_df = pd.read_csv(f)
-        print(climbs_df.iloc[0]['climb_type'])
         climbs_data = climbs_df.to_dict('records')
     for entry in climbs_data:
         climbs.replace_one(entry, entry, upsert=True)
