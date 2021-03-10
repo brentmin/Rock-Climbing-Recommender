b4af3fcb56c3a45f02a4393b0091849ad06cfdd7
Brent Min <brentmin@Brents-MacBook-Pro.local>
2021-03-09 18:09:38 -0800

    bug testing

--- src/data/run_data.py
+++ src/data/run_data.py
@@ -21,7 +21,7 @@ def run_data(data_params, args):
                                 run data scraping/cleaning code
     """
     #the mongo connection
-    client_url = 'mongodb+srv://DSC102:KcPlrKh0wFH6W5Yh@cluster0.4gstr.mongodb.net/MountainProject?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE'
+    client_url = 'mongodb+srv://DSC102:Kw4ngOgiLl6mPi5r@cluster0.4gstr.mongodb.net/MountainProject?retryWrites=true&w=majority'
     my_client = MongoClient(client_url)
 
     # first check that we want to run some data scraping/cleaning code
